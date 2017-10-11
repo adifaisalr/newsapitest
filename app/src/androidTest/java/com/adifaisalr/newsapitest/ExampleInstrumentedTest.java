9346841161ea6f3b67703414e87364a0b1dfb37c
Adi Faisal <adi.faisal@reservasi.com>
2017-10-11 14:06:49 +0700

    fix instrumented test

--- app/src/androidTest/java/com/adifaisalr/newsapitest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/adifaisalr/newsapitest/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.adifaisalr.tokopediatest", appContext.getPackageName());
+        assertEquals("com.adifaisalr.newsapitest", appContext.getPackageName());
     }
 }
